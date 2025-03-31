f5cca349d7e2dc9565ceb98a927439e46daf61d2
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-03-31 03:52:54 +0000

    commit

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,18 @@ public class PracticeProblem {
 
 	}
 
+	public static void insertionSort(char[] characters){
+		int n = characters.length;
+
+		for (int i = 0; i < n; i++){
+			char key = characters[i];
+			int index = i - 1;
+			while (index >= 0 && characters[index] > key){
+				characters[index + 1] = characters[index];
+				index -= 1;
+			}
+			characters[index + 1] = key;
+		}
+	}
 
 }
